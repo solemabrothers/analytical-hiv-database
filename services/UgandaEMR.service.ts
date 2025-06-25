6a0a2e745fe490088e95ca93a7751ef24538dbec
solema <solemasolomon@gmail.com>
2025-06-25 14:31:18 +0300

    Add raw fhir

--- services/UgandaEMR.service.ts
+++ services/UgandaEMR.service.ts
@@ -2,11 +2,11 @@ import { ServiceSchema, Context } from "moleculer";
 import { Pool } from "pg";
 
 const pool = new Pool({
-	user: process.env.PG_USER,
-	password: process.env.PG_PASSWORD,
-	host: process.env.PG_HOST,
+	user: process.env.PG_USER_DWH,
+	password: process.env.PG_PASSWORD_DWH,
+	host: process.env.PG_HOST_DWH,
 	port: process.env.PG_PORT ? Number(process.env.PG_PORT) : 5432,
-	database: process.env.PG_DATABASE,
+	database: process.env.PG_DATABASE_DWH,
 	max: 20,
 	idleTimeoutMillis: 30000,
 	connectionTimeoutMillis: 2000,
@@ -29,10 +29,22 @@ const RawFhirService: ServiceSchema = {
 			async handler(ctx: Context<Record<string, any>, CustomMeta>) {
 				const rawFhirBundle = ctx.params;
 
+				const now = new Date();
+
 				try {
 					await pool.query(
-						`INSERT INTO emr_fhir_resource_bundles (source, data) VALUES ($1, $2)`,
-						[ctx.meta?.source || "UgandaEMR", rawFhirBundle]
+						`INSERT INTO "import".message_resource (
+							resource_published,
+							resource_updated,
+							resource_data,
+							source_system
+						) VALUES ($1, $2, $3, $4)`,
+						[
+							now,
+							now,
+							JSON.stringify(rawFhirBundle),
+							ctx.meta?.source || "UgandaEMR"
+						]
 					);
 					return { success: true, message: "FHIR bundle saved successfully." };
 				} catch (error) {
